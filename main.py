bd8b5fcf7d9385813884d7a361ca0b2f37b9e8ad
Artur Z <arturziianbaev@gmail.com>
2023-11-03 21:50:47 -0700

    insert data funct

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ db=sqlite3.connect("users.db")
 
 cursor=db.cursor()
 #create table
-cursor.execute("""
-        CREATE TABLE users (
-            login TEXT PRIMARY KEY,
-            password TEXT
-        )
-""")
+# cursor.execute("""
+#         CREATE TABLE users (
+#             login TEXT PRIMARY KEY,
+#             password TEXT
+#         )
+# """)
+
+
+def insert_data(login,password):
+    with db:
+        cursor.execute("INSERT INTO users VALUES (?,?)",(login,password))
+
+
+insert_data('admin','admin')
