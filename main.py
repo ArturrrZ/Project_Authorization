9d7268a9933d414ba9f6bfe73c022ec51f6b37a1
Artur Z <arturziianbaev@gmail.com>
2023-11-03 21:48:22 -0700

    create table

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ import sqlite3
 db=sqlite3.connect("users.db")
 
 cursor=db.cursor()
-
-
+#create table
+cursor.execute("""
+        CREATE TABLE users (
+            login TEXT PRIMARY KEY,
+            password TEXT
+        )
+""")
